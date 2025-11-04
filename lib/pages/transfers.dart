2b31e446bd1316ef947acfe24ad9042827d8b7ab
manfrin <manfredd_10@hotmail.com>
2025-11-04 19:24:51 -0300

    feat: singleChildscrollView

--- lib/pages/transfers.dart
+++ lib/pages/transfers.dart
@@ -56,121 +56,127 @@ class _TransfersState extends State<Transfers> {
         ),
       ),
       padding: const EdgeInsets.only(top: 32.0, left: 32.0, right: 32.0),
-      child: Center(
-        child: Column(
-          children: [
-            Text(
-              'Nova Transação',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color(int.parse('0xFF${titleFontColor.substring(1)}')),
-              ),
-            ),
-            const SizedBox(height: 32),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Color(
-                    int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
-                  ),
-                ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  value: selectedTransactionType,
-                  hint: const Text('Selecione o tipo de transação'),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedTransactionType = newValue!;
-                    });
-                  },
-                  items: transferOptions.map<DropdownMenuItem<String>>((
-                    String value,
-                  ) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  dropdownColor: Colors.white,
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'Nova Transação',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(int.parse('0xFF${titleFontColor.substring(1)}')),
                 ),
               ),
-            ),
-            const SizedBox(height: 32),
-            Text(
-              'Valor',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Color(int.parse('0xFF${titleFontColor.substring(1)}')),
-              ),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 200,
-              child: TextField(
-                controller: amountController,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(regexForAmount)),
-                ],
-                decoration: InputDecoration(
-                  hintText: '00,00',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(
-                        int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
-                      ),
-                      width: 1,
+              const SizedBox(height: 32),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Color(
+                      int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(
-                        int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
-                      ),
-                      width: 1,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: handleTransaction,
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 16,
-                ),
-                backgroundColor: Color(
-                  int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
-                ),
-                foregroundColor: Color(
-                  int.parse('0xFF${buttonFontColor.substring(1)}'),
-                ),
-                textStyle: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w200,
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: selectedTransactionType,
+                    hint: const Text('Selecione o tipo de transação'),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedTransactionType = newValue!;
+                      });
+                    },
+                    items: transferOptions.map<DropdownMenuItem<String>>((
+                      String value,
+                    ) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    dropdownColor: Colors.white,
+                  ),
                 ),
               ),
-              child: const Text(
-                'Concluir\ntransação',
-                textAlign: TextAlign.center,
+              const SizedBox(height: 32),
+              Text(
+                'Valor',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(int.parse('0xFF${titleFontColor.substring(1)}')),
+                ),
               ),
-            ),
-            const SizedBox(height: 32),
-          ],
+              const SizedBox(height: 16),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(regexForAmount)),
+                  ],
+                  decoration: InputDecoration(
+                    hintText: '00,00',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(
+                          int.parse(
+                            '0xFF${buttonBackgroundColor.substring(1)}',
+                          ),
+                        ),
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(
+                          int.parse(
+                            '0xFF${buttonBackgroundColor.substring(1)}',
+                          ),
+                        ),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: handleTransaction,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 16,
+                  ),
+                  backgroundColor: Color(
+                    int.parse('0xFF${buttonBackgroundColor.substring(1)}'),
+                  ),
+                  foregroundColor: Color(
+                    int.parse('0xFF${buttonFontColor.substring(1)}'),
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                child: const Text(
+                  'Concluir\ntransação',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
         ),
       ),
     );
