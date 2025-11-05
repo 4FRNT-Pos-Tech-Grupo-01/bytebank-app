baf3e5785b7b257e87457d7f4810dd6ce839611f
Simone <simone.l.silva@parceiros.nos.pt>
2025-11-05 21:11:36 +0000

    removing debug stick from app

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ByteBank App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.cyan),
       ),
